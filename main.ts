80846bb8ee55e3a437e734f6dff1f492c7722bc2
rogergimenohernandez <rogergimenohernandez@gmail.com>
2025-11-25 06:57:19 +0100

    Actualizar pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,35 @@
 namespace SpriteKind {
     export const SuperFood = SpriteKind.create()
+    export const Wall = SpriteKind.create()
 }
 function CreaPacMan () {
-    PacManSprite = sprites.create(assets.image`PacMan`, SpriteKind.Player)
-    animation.runImageAnimation(
-    PacManSprite,
-    assets.animation`PacManAnimR`,
-    200,
-    true
-    )
+    pacRight = [assets.image`PacManPetitR1`, assets.image`PacManPetitR2`]
+    pacLeft = [assets.image`PacManPetitL1`, assets.image`PacManPetitL2`]
+    pacUp = [assets.image`PacManPetitU1`, assets.image`PacManPetitU2`]
+    pacDown = [assets.image`PacManPetitD1`, assets.image`PacManPetitD2`]
+    pacManActual = pacRight
+    PacManSprite = sprites.create(pacManActual[contadorMov], SpriteKind.Player)
+    PacManSprite.setStayInScreen(true)
     PacManSprite.z = 100
-    PacManSprite.setPosition(randint(2, 8) * 16 + 8, randint(2, 5) * 16 + 8)
+    PacManSprite.setPosition(iniPacManPos[0] * pas + pas / 2, iniPacManPos[1] * pas + pas / 2)
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    PacManSprite,
-    assets.animation`PacManAnimD`,
-    200,
-    true
-    )
+    // animation.runImageAnimation(PacManSprite, assets.animation`PacManAnimD`, 200, true)
+    pacManActual = pacDown
+    PacManSprite.setImage(pacManActual[contadorMov])
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    PacManSprite,
-    assets.animation`PacManAnimR`,
-    200,
-    true
-    )
+    // animation.runImageAnimation(PacManSprite, assets.animation`PacManAnimR`, 200, true)
+    pacManActual = pacRight
+    PacManSprite.setImage(pacManActual[contadorMov])
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    PacManSprite,
-    assets.animation`PacManAnimL`,
-    200,
-    true
-    )
+    // animation.runImageAnimation(PacManSprite, assets.animation`PacManAnimL`, 200, true)
+    pacManActual = pacLeft
+    PacManSprite.setImage(pacManActual[contadorMov])
 })
 function moureFantasma (Fantasma: Sprite, PacMan: Sprite) {
+    Fantasma.setImage(FantasmaActual[contadorMovFantasma])
     movX2 = 0
     movY2 = 0
     diferenciaX = PacMan.x - Fantasma.x
@@ -70,12 +63,19 @@ function moureFantasma (Fantasma: Sprite, PacMan: Sprite) {
             }
         }
     }
-    newLocFantasmaX = Fantasma.x + movX2 * 16
-    newLocFantasmaY = Fantasma.y + movY2 * 16
-    if (!(tiles.tileAtLocationIsWall(tiles.getTileLocation(newLocFantasmaX / 16, newLocFantasmaY / 16)))) {
+    newLocFantasmaX = Fantasma.x + movX2 * pas
+    newLocFantasmaY = Fantasma.y + movY2 * pas
+    indexX = (newLocFantasmaX - 4) / pas
+    indexY = (newLocFantasmaY - 4) / pas
+    if (mapa[indexY][indexX] != "1") {
         Fantasma.x = newLocFantasmaX
         Fantasma.y = newLocFantasmaY
     }
+}
+function crearParet (x: number, y: number) {
+    wall = sprites.create(assets.image`Paret`, SpriteKind.Wall)
+    wall.setPosition(x, y)
+    wall.z = 100
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (PaMan, Fantasma) {
     if (modeSuperSayanBlue) {
@@ -89,6 +89,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (PaMan, Fantasma
     }
 })
 function mourePacMan (PacMan: Sprite) {
+    PacMan.setImage(pacManActual[contadorMov])
     movX = 0
     movY = 0
     if (controller.left.isPressed() && !(controller.right.isPressed()) && !(controller.up.isPressed()) && !(controller.down.isPressed())) {
@@ -103,75 +104,116 @@ function mourePacMan (PacMan: Sprite) {
     if (!(controller.left.isPressed()) && !(controller.right.isPressed()) && !(controller.up.isPressed()) && controller.down.isPressed()) {
         movY = 1
     }
-    newLocPacManX = PacMan.x + movX * 16
-    newLocPacManY = PacMan.y + movY * 16
-    if (!(tiles.tileAtLocationIsWall(tiles.getTileLocation(newLocPacManX / 16, newLocPacManY / 16)))) {
+    newLocPacManX = PacMan.x + movX * pas
+    newLocPacManY = PacMan.y + movY * pas
+    indexX2 = (newLocPacManX - 4) / pas
+    indexY2 = (newLocPacManY - 4) / pas
+    if (mapa[indexY2][indexX2] != "1") {
         PacMan.x = newLocPacManX
         PacMan.y = newLocPacManY
     }
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    PacManSprite,
-    assets.animation`PacManAnimU`,
-    200,
-    true
-    )
+    // animation.runImageAnimation(PacManSprite, assets.animation`PacManAnimU`, 200, true)
+    pacManActual = pacUp
+    PacManSprite.setImage(pacManActual[contadorMov])
 })
-function CreaMenjar () {
-    for (let índiceColumna = 0; índiceColumna <= 7; índiceColumna++) {
-        for (let índiceFila = 0; índiceFila <= 4; índiceFila++) {
-            FoodSprite = sprites.create(assets.image`Menjar`, SpriteKind.Food)
-            FoodSprite.setPosition((índiceColumna + 1) * 16 + 8, (índiceFila + 1) * 16 + 8)
+function CreaMenjar (x: number, y: number) {
+    FoodSprite = null
+FoodSprite = sprites.create(assets.image`Moneda`, SpriteKind.Food)
+    FoodSprite.setPosition(x, y)
+    FoodSprite.z = 10
+    return FoodSprite
+}
+function CreaSuperMenjar (x: number, y: number) {
+    SuperMenjar = null
+SuperMenjar = sprites.create(assets.image`superMoneda`, SpriteKind.Food)
+    SuperMenjar.setPosition(x, y)
+    SuperMenjar.z = 10
+    return SuperMenjar
+}
+function CreaMapa () {
+    mapa = [
+    "11111111111111111111",
+    "1......1.1.1.......1",
+    "1.1111.1.1.1.11111.1",
+    "1X1..............1X1",
+    "1.1.1.11F  111.1.1.1",
+    "1...1.11   1.1.1...1",
+    "111....11111.....111",
+    "....11.1.1.111.1....",
+    "111..............111",
+    "1...1111111.1111...1",
+    "1.1......P.......1.1",
+    "1X11.111..11.11111X1",
+    "1.11.1....1.....11.1",
+    "1......11...111....1",
+    "11111111111111111111"
+    ]
+    for (let y = 0; y <= mapa.length - 1; y++) {
+        for (let x = 0; x <= mapa[y].length - 1; x++) {
+            if (mapa[y][x] == "1") {
+                crearParet(x * pas + pas / 2, y * pas + pas / 2)
+            }
+            if (mapa[y][x] == ".") {
+                let Menjars: Sprite[] = []
+                Menjars.push(CreaMenjar(x * pas + pas / 2, y * pas + pas / 2))
+            }
+            if (mapa[y][x] == "X") {
+                let SuperMenjars: Sprite[] = []
+                SuperMenjars.push(CreaSuperMenjar(x * pas + pas / 2, y * pas + pas / 2))
+            }
+            if (mapa[y][x] == "P") {
+                iniPacManPos = [x, y]
+            }
+            if (mapa[y][x] == "F") {
+                iniFantasmaPos = [x, y]
+            }
         }
     }
 }
-function CreaSuperMenjar () {
-    SuperMenjar = sprites.create(assets.image`SuperMenjar`, SpriteKind.SuperFood)
-    SuperMenjar.setPosition(randint(2, 8) * 16 + 8, randint(2, 5) * 16 + 8)
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (PaMan, Menjar) {
-    Menjar.destroy()
-    music.baDing.play()
-    info.changeScoreBy(10)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.SuperFood, function (PaMan, SuperMenjar) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.SuperFood, function (PacMan, SuperMenjar) {
     SuperMenjar.destroy()
-    animation.runImageAnimation(
-    GhostSprite,
-    assets.animation`FantasmaAnimPor`,
-    200,
-    true
-    )
+    FantasmaActual = FantasmaPor
+    GhostSprite.setImage(FantasmaActual[contadorMovFantasma])
     tempsPoder = 5000
     modeSuperSayanBlue = true
     tempsActualitzacioFantasma = 500
     music.baDing.play()
     info.changeScoreBy(50)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (PaMan, Menjar) {
+    Menjar.destroy()
+    music.baDing.play()
+    info.changeScoreBy(10)
+})
 function CreaEnemig () {
     // for (let i = 0; i <= 3; i++) {
     // ghosts.push(crearFantasma(i))
     // }
-    GhostSprite = sprites.create(assets.image`Fantasma`, SpriteKind.Enemy)
-    animation.runImageAnimation(
-    GhostSprite,
-    assets.animation`FantasmaAnim`,
-    200,
-    true
-    )
+    Fantasma = [assets.image`FantasmaPetit1`, assets.image`FantasmaPetit2`]
+    FantasmaPor = [assets.image`FantasmaPetitPor1`, assets.image`FantasmaPetitPor2`]
+    FantasmaActual = Fantasma
+    GhostSprite = sprites.create(FantasmaActual[contadorMovFantasma], SpriteKind.Enemy)
     PacManSprite.setBounceOnWall(true)
-    GhostSprite.setPosition(randint(2, 8) * 16 + 8, randint(2, 5) * 16 + 8)
+    GhostSprite.setPosition(iniFantasmaPos[0] * pas + pas / 2, iniFantasmaPos[1] * pas + pas / 2)
     GhostSprite.z = 100
 }
+let Fantasma: Image[] = []
 let tempsPoder = 0
 let GhostSprite: Sprite = null
-let SuperMenjar: Sprite = null
-let FoodSprite: Sprite = null
+let FantasmaPor: Image[] = []
+let iniFantasmaPos: number[] = []
+let indexY2 = 0
+let indexX2 = 0
 let newLocPacManY = 0
 let newLocPacManX = 0
 let movY = 0
 let movX = 0
+let wall: Sprite = null
+let mapa: string[] = []
+let indexY = 0
+let indexX = 0
 let newLocFantasmaY = 0
 let newLocFantasmaX = 0
 let modeSuperSayanBlue = false
@@ -179,15 +221,27 @@ let diferenciaY = 0
 let diferenciaX = 0
 let movY2 = 0
 let movX2 = 0
+let contadorMovFantasma = 0
+let FantasmaActual: Image[] = []
+let iniPacManPos: number[] = []
+let contadorMov = 0
 let PacManSprite: Sprite = null
+let pacManActual: Image[] = []
+let pacDown: Image[] = []
+let pacUp: Image[] = []
+let pacLeft: Image[] = []
+let pacRight: Image[] = []
 let tempsActualitzacioFantasma = 0
+let pas = 0
+let menjar2 = null
+let FoodSprite: Sprite = null
+let SuperMenjar: Sprite = null
+pas = 8
 tempsActualitzacioFantasma = 300
 let tempsActualitzacio = 200
-tiles.setCurrentTilemap(tilemap`nivel1`)
+CreaMapa()
 CreaPacMan()
 CreaEnemig()
-CreaMenjar()
-CreaSuperMenjar()
 game.onUpdate(function () {
     if (sprites.allOfKind(SpriteKind.Food).length == 0 && sprites.allOfKind(SpriteKind.SuperFood).length == 0) {
         game.over(true, effects.confetti)
@@ -199,19 +253,17 @@ game.onUpdateInterval(tempsActualitzacio, function () {
         if (tempsPoder <= 0) {
             modeSuperSayanBlue = false
             tempsActualitzacioFantasma = 200
-            animation.runImageAnimation(
-            GhostSprite,
-            assets.animation`FantasmaAnim`,
-            200,
-            true
-            )
+            FantasmaActual = Fantasma
+            GhostSprite.setImage(FantasmaActual[contadorMov])
             music.powerDown.play()
         }
     }
 })
 game.onUpdateInterval(tempsActualitzacio, function () {
+    contadorMov = (contadorMov + 1) % 2
     mourePacMan(PacManSprite)
 })
 game.onUpdateInterval(tempsActualitzacioFantasma, function () {
+    contadorMovFantasma = (contadorMovFantasma + 1) % 2
     moureFantasma(GhostSprite, PacManSprite)
 })
